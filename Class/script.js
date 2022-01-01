c844931032c4e46573aa357ebdca36ccf531b195
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2021-12-31 19:24:12 -0500

    Add files via upload

---
+++ Class/script.js
@@ -0,0 +1,15 @@
+import backpack from "./backpack.js";
+const everydaybackpack = new backpack(
+    "BP1",
+    30,
+    "blue",
+    5,
+    10,
+    10,
+    false
+);
+ 
+console.log("backpack object is: ",everydaybackpack);
+console.log("color ",everydaybackpack.color);
+
+
